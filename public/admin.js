e5fb97c59691b8cf11de1eb6d14250b69701b63c
Wolfe-Edelman <edelman.wolfe@gmail.com>
2022-12-10 06:40:47 -0500

    my first attempt at JS-Building-an-Admin-Portal

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,30 @@
+async function main() {
+    let response = await fetch ('http://localhost:3001/listBooks')
+    let books = await response.json()
+    books.forEach(updateBook)
+}
+function updateBook(book) {
+    let root = document.querySelector('#root')
+    let li = document.createElement('li')
+    li.textContent = book.title
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+    let button = document.createElement('button')
+    button.textContent = 'save'
+    button.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', (
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
 
-// Your Code Here
+        ))
+    })
+    li.append(quantityInput, button)
+    root.append(li)
+}
+main();
